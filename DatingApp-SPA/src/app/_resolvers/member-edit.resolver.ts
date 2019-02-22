a6f2c3535fce745fbd0f11a6b7a302a86e4e9923
Luís Miguel Pardelha Correia <luis.correia@aitec.pt>
2019-02-22 16:32:05 +0000

    end of section 10

---
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Route, ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable()
+export class MemberEditResolver implements Resolve<User> {
+    constructor (private userService: UserService, private router: Router,
+        private alertify: AlertifyService, private authservice: AuthService) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(this.authservice.decodedToken.nameid).pipe(
+            catchError(error => {
+                this.alertify.error('Problem retrieving your data');
+                this.router.navigate(['/members']);
+                return of(null);
+            })
+        );
+    }
+}
